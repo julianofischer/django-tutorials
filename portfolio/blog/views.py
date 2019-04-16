3412e9274270389b4d793f6e6477a4771da6fe8d
Juliano Fischer Naves <julianofischer@gmail.com>
2019-04-16 17:15:38 -0400

    blog detail

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 def allblogs(request):
     posts = Post.objects.all()
     return render(request, 'blog/allblogs.html', {'posts':posts})
+
+def detail(request, blog_id):
+    post = get_object_or_404(Post, pk=blog_id)
+    return render(request, 'blog/detail.html', {'post':post})
